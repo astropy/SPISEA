b38c786278191bd2b48942cb20b36966fde0bbb9
Matt Hosek <mwhosek@ifa.hawaii.edu>
2015-08-07 14:31:46 -1000

    Added Rieke+Lebofsky+85 reddening law

--- popstar/reddening.py
+++ popstar/reddening.py
@@ -174,7 +174,6 @@ class RedLawCardelli(pysynphot.reddening.CustomRedLaw):
         Av_Aks = 1.0 / Aks_Av 
         
         output = extinction * Av_Aks # If Aks = 1, Av/Aks = Av
-        
         return output
 
 
@@ -241,3 +240,72 @@ class RedLawRomanZuniga07(pysynphot.reddening.CustomRedLaw):
 
         return A_at_wave
     
+class RedLawRiekeLebofsky(pysynphot.reddening.CustomRedLaw):
+    """
+    An object that represents the reddening vs. wavelength for the 
+    Rieke+Lebofsky+85 reddening law. The returned object is 
+    pysynphot.reddenining.CustomRedLaw (ArraySpectralElement)
+
+    The wavelength range over which this law is calculated is
+    0.365 - 13 microns.
+    """
+    def __init__(self):
+        # Fetch the extinction curve, pre-interpolate across 0.365-13 microns
+        wave = np.arange(0.365, 13.0, 0.001)
+        
+        # This will eventually be scaled by AKs when you
+        # call reddening(). Right now, calc for AKs=1
+        Alambda_scaled = RedLawRiekeLebofsky.RiekeLebofsky(wave, 1.0)
+
+        # Convert wavelength to angstrom
+        wave *= 10 ** 4
+
+        pysynphot.reddening.CustomRedLaw.__init__(self, wave=wave, 
+                                                  waveunits='angstrom',
+                                                  Avscaled=Alambda_scaled,
+                                                  name='RiekeLebofsky',
+                                                  litref='Rieke+Lebovsky 1985')
+
+    @staticmethod
+    def RiekeLebofsky(wavelength, AKs, makePlot=False):
+        """
+        Calculate the resulting extinction for an array of wavelengths.
+        The extinction is normalized with A_Ks.
+
+        Data pulled from Rieke+Lebofsky 1985, Table 3
+        """
+        filters = ['U', 'B', 'V', 'R', 'I', 'J', 'H', 'K', 'L', 'M', 'N', 
+                   '[8.0]', '[8.5]', '[9.0]', '[9.5]', '[10.0]', '[10.5]', 
+                   '[11.0]', '[11.5]', '[12.0]', '[12.5]', '[13.0]']
+        wave = np.array([0.365, 0.445, 0.551, 0.658, 0.806, 1.25, 1.635, 2.2, 
+                         3.77, 4.68, 4.75, 8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0,
+                        11.5, 12.0, 12.5, 13.0])
+        A_Av = np.array([1.531, 1.324, 1.00, 0.748, 0.482, 0.282, 0.175, 0.112,
+                         0.058, 0.023, 0.052, 0.02, 0.043, 0.074, 0.087, 0.083,
+                         0.074, 0.060, 0.047, 0.037, 0.030, 0.027])
+        # Want to change this from A/Av to A/AK
+        k_ind = np.where(np.array(filters) == 'K')
+        Ak_Av = A_Av[k_ind]
+        Av_Ak = 1.0 / Ak_Av
+
+        A_Ak = A_Av * Av_Ak
+        
+        # Interpolate over the curve
+        spline_interp = interpolate.splrep(wave, A_Ak, k=3, s=0)
+
+        A_Ak_at_wave = interpolate.splev(wavelength, spline_interp)
+        A_at_wave = AKs * A_Ak_at_wave
+
+        if makePlot:
+            py.clf()
+            py.plot(wave, A_Ak, 'r.', ms = 8)
+
+            # Make an interpolated curve.
+            wavePlot = np.arange(wave.min(), wave.max(), 0.1)
+            extPlot = interpolate.splev(wavePlot, spline_interp)
+            py.plot(wavePlot, extPlot, 'k-')
+            py.xlabel('Wavelength (microns)')
+            py.ylabel('Extinction (magnitudes)')
+            py.title('Rieke+Lebofsky 1985')
+
+        return A_at_wave
