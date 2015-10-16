cab719cf95ee0c9d6e78e9bc3915e3d1d4acea40
Jessica Lu <jlu.astro@yahoo.com>
2015-10-15 16:10:17 -1000

    Added data holder objects.

---
+++ popstar/utils/objects.py
@@ -0,0 +1,9 @@
+class DataHolder(object):
+    """
+    This is a generic data holder object. You can
+    create any thing and it can hang off this.
+
+    Why do we need this again?
+    """
+    def __init__(self):
+        return
