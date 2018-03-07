46c392a449c0f944931ddad1b780fa0a8bf27aa7
Casey Lam <casey@galley.astro.berkeley.edu>
2018-03-07 00:00:14 -0800

    added a new class IFMF_new that does the same thing as before, but elimiates for loops

--- popstar/ifmf.py
+++ popstar/ifmf.py
@@ -144,3 +144,124 @@ class IFMF(object):
                 output_array[0][i] = -99
                 output_array[1][i] = -1
         return output_array
+
+class IFMF_new(object):
+    def __init__(self):
+        """
+        The IFMF class, without all the for loops
+        """
+        pass
+
+    def BH_mass_core_low(self, MZAMS):
+        """                                                                                                      
+        Eqn (1)                                                                                                  
+        Paper: 15 < MZAMS < 40                                                                                   
+        Us extending: 15 < MZAMS < 42.22                                                                         
+        """
+        return -2.024 + 0.4130*MZAMS
+
+    def BH_mass_all_low(self, MZAMS):
+        """                                                                                                      
+        Eqn (2)                                                                                                  
+        Paper: 15 < MZAMS < 40                                                                                   
+        Us extending: 15 < MZAMS < 42.22                                                                         
+        """
+        return 16.28 + 0.00694 * (MZAMS - 21.872) - 0.05973 * (MZAMS - 21.872)**2 + 0.003112 * (MZAMS - 21.872)**3
+
+    def BH_mass_high(self, MZAMS):
+        """                                                                                                      
+        Eqn (3)                                                                                                  
+        Paper: 45 < MZAMS < 120                                                                                  
+        Us extending: 42.22 < MZAMS < 120                                                                        
+        """
+        return 5.795 + 1.007 * 10**9 * MZAMS**-4.926
+
+    def BH_mass_low(self, MZAMS, f_ej):
+        """                                                                                                      
+        Eqn (4)                                                                                                  
+        Paper: 15 < MZAMS < 40                                                                                   
+        Us extending: 15 < MZAMS < 42.22                                                                         
+        """
+        return f_ej * self.BH_mass_core_low(MZAMS) + (1 - f_ej) * self.BH_mass_all_low(MZAMS)
+
+    def NS_mass(self, MZAMS):
+        """                                                                                                      
+        Paper: 9 < MZAMS 120                                                                                     
+        Simplify to just return one value                                                                        
+        """
+        return 1.6 * np.ones(len(MZAMS))
+
+    def WD_mass(self, MZAMS):
+        """                                                                                                      
+        From Kalirai+07                                                                                          
+        1.16 < MZAMS < 6.5                                                                                       
+        FIXME: need to extend these ranges...                                                                    
+        """
+        return 0.109*MZAMS + 0.394
+
+    def generate_death_mass_distribution(self, mass_array):
+        output_array = np.zeros((2, len(mass_array)))
+
+        random_array = np.random.randint(1, 101, size = len(mass_array))
+
+        id_array0 = np.where((mass_array < 0.5) | (mass_array >= 120))
+        output_array[0][id_array0] = -99 * np.ones(len(id_array0))
+        output_array[1][id_array0]  = -1 * np.ones(len(id_array0))
+
+        id_array1 = np.where((mass_array >= 0.5) & (mass_array < 9))
+        output_array[0][id_array1] = self.WD_mass(mass_array[id_array1])
+        output_array[1][id_array1]= 1 * np.ones(len(id_array1))
+
+        id_array2 = np.where((mass_array >= 9) & (mass_array < 15))
+        output_array[0][id_array2] = self.NS_mass(mass_array[id_array2])
+        output_array[1][id_array2] = 2 * np.ones(len(id_array2))
+
+        id_array3_BH = np.where((mass_array >= 15) & (mass_array < 17.8) & (random_array > 68))
+        output_array[0][id_array3_BH] = self.BH_mass_low(mass_array[id_array3_BH], 0.9)
+        output_array[1][id_array3_BH] = 3 * np.ones(len(id_array3_BH))
+
+        id_array3_NS = np.where((mass_array >= 15) & (mass_array < 17.8) & (random_array <= 68))
+        output_array[0][id_array3_NS] = self.NS_mass(mass_array[id_array3_NS])
+        output_array[1][id_array3_NS] = 2 * np.ones(len(id_array3_NS))
+
+        id_array4_BH = np.where((mass_array >= 17.8) & (mass_array < 18.5) & (random_array > 83))
+        output_array[0][id_array4_BH]= self.BH_mass_low(mass_array[id_array4_BH], 0.9)
+        output_array[1][id_array4_BH] = 3 * np.ones(len(id_array4_BH))
+        id_array4_NS = np.where((mass_array >= 17.8) & (mass_array < 18.5) & (random_array <= 83))
+        output_array[0][id_array4_NS] = self.NS_mass(mass_array[id_array4_NS])
+        output_array[1][id_array4_NS] = 2 * np.ones(len(id_array4_NS))
+
+        id_array5_BH = np.where((mass_array >= 18.5) & (mass_array < 21.7) & (random_array > 50))
+        output_array[0][id_array5_BH] = self.BH_mass_low(mass_array[id_array5_BH], 0.9)
+        output_array[1][id_array5_BH] = 3 * np.ones(len(id_array5_BH))
+        id_array5_NS = np.where((mass_array >= 18.5) & (mass_array < 21.7) & (random_array <= 50))
+        output_array[0][id_array5_NS] = self.NS_mass(mass_array[id_array5_NS])
+        output_array[1][id_array5_NS] = 2 * np.ones(len(id_array5_NS))
+
+        id_array6 = np.where((mass_array >= 21.7) & (mass_array < 25.2))
+        output_array[0][id_array6] = self.BH_mass_low(mass_array[id_array6], 0.9)
+        output_array[1][id_array6]= 3 * np.ones(len(id_array6))
+
+        id_array7_BH = np.where((mass_array >= 25.2) & (mass_array < 27.5) & (random_array > 65))
+        output_array[0][id_array7_BH] = self.BH_mass_low(mass_array[id_array7_BH], 0.9)
+        output_array[1][id_array7_BH] = 3 * np.ones(len(id_array7_BH))
+        id_array7_NS = np.where((mass_array >= 25.2) & (mass_array < 27.5) & (random_array <= 65))
+        output_array[0][id_array7_NS] = self.NS_mass(mass_array[id_array7_NS])
+        output_array[1][id_array7_NS] = 2 * np.ones(len(id_array7_NS))
+
+        id_array8 = np.where((mass_array >= 27.5) & (mass_array < 42.22))
+        output_array[0][id_array8] = self.BH_mass_low(mass_array[id_array8], 0.9)
+        output_array[1][id_array8] = 3 * np.ones(len(id_array8))
+
+        id_array9 = np.where((mass_array >= 42.22) & (mass_array < 60))
+        output_array[0][id_array9] = self.BH_mass_high(mass_array[id_array9])
+        output_array[1][id_array9] = 3 * np.ones(len(id_array9))
+
+        id_array10_BH = np.where((mass_array >= 60) & (mass_array < 120) & (random_array > 40))
+        output_array[0][id_array10_BH] = self.BH_mass_high(mass_array[id_array10_BH])
+        output_array[1][id_array10_BH] = 3 * np.ones(len(id_array10_BH))
+        id_array10_NS = np.where((mass_array >= 60) & (mass_array < 120) & (random_array <= 40))
+        output_array[0][id_array10_NS] = self.NS_mass(mass_array[id_array10_NS])
+        output_array[1][id_array10_NS] = 2 * np.ones(len(id_array10_NS))
+
+        return(output_array)
