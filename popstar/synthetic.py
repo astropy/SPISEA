8a15527d4f858d465cc371fcd3f9d51e6d3d7424
Matt Hosek <mwhosek@spray.astro.berkeley.edu>
2018-04-03 15:05:24 -0700

    Fixed WR support for MISTv1 isochrones

--- popstar/synthetic.py
+++ popstar/synthetic.py
@@ -638,6 +638,9 @@ class Isochrone(object):
         if 'logT_WR' in keys:
             evol['isWR'] = evol['logT'] != evol['logT_WR']
             isWR_all = evol['isWR']
+        elif 'phase' in keys:
+            evol['isWR'] = evol['phase'] == 9
+            pdb.set_trace()
         else:
             isWR_all = ['None'] * len(evol)
 
