a73903005d90bfe1308fc3e69ba82f9786e18103
Matt Hosek <mwhosek@ifa.hawaii.edu>
2019-11-08 13:16:11 -0800

    Sphinx extensions arent working for readthedocs but I dont understand why. Ive removed all but the autodoc here.

--- docs/conf.py
+++ docs/conf.py
@@ -32,14 +32,7 @@ sys.path.append('/Users/mwhosek/code/python/PyPopStar/popstar')
 #              'sphinx.ext.inheritance_diagram',
 #              'numpydoc',
 #              'sphinx.ext.autodoc']
-extensions = ['IPython.sphinxext.ipython_directive',
-              'IPython.sphinxext.ipython_console_highlighting',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.autodoc',
-              'sphinx.ext.doctest',
-              'sphinx.ext.inheritance_diagram',
-              'numpydoc',
-              'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
