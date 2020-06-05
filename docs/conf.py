1443305e67118b4fbf847e594f03f853698794f0
Matt Hosek <mwhosek@ifa.hawaii.edu>
2020-06-05 09:29:39 -0700

    Updating version number to 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Matthew Hosek Jr, Jessica R. Lu, Casey Y. Lam'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0b1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
