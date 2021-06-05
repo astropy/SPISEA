dd46e83043dfd547d0c579892d24131250b3133c
Ryota Inagaki <66146094+RyotaInagaki1@users.noreply.github.com>
2021-06-04 19:55:24 -0700

    Update test_BPASS_cluster.py

--- spisea/tests/test_BPASS_cluster.py
+++ spisea/tests/test_BPASS_cluster.py
@@ -243,20 +243,20 @@ def test_isochrones_dim_stars():
     assert all(dim_secondaries['mass'] < 1.0)
 
 
-def test_cluster_mass_vs_expected():
+def test_cluster_2():
     # Check if the evolution class works fine
     # BPASS Isochrone used to create cluster
-    iso1 = synthetic.Isochrone_Binary(9.2, 0.5, 2000,
-                                  math.log10(0.1),
+    iso1 = synthetic.Isochrone_Binary(9.2, 0.5, 8000,
+                                  math.log10(1),
                                   mass_sampling=1)
     # MIST v1 Isochrone used to create cluster
-    iso2 = synthetic.IsochronePhot(9.2, 0.5, 2000,
-                                   math.log10(0.1),
+    iso2 = synthetic.IsochronePhot(9.2, 0.5, 8000,
+                                   math.log10(1),
                                    recomp=True)
     clus_1 = synthetic.Cluster_w_Binaries(iso1,
                                           imf.imf.IMFSalpeter1955(multiplicity=
                                                           imf.multiplicity.
-                                                          MultiplicityResolvedDK()),
+                                                          MultiplicityUnresolved()),
                                       200000, ifmr=ifmr.IFMR_Spera15())
     clus_2 = synthetic.ResolvedCluster(iso2,
                                        imf.imf.IMFSalpeter1955(multiplicity=imf. multiplicity.
@@ -272,22 +272,247 @@ def test_cluster_mass_vs_expected():
                                              (imf.multiplicity.
                                               MultiplicityResolvedDK()))
     clus_1 = synthetic.Cluster_w_Binaries(iso1, custom_IMF,
-                                      200000, ifmr=ifmr.IFMR_Spera15())
+                                      1000000, ifmr=ifmr.IFMR_Spera15())
     clus_2 = synthetic.ResolvedCluster(iso2, custom_IMF,
-                                       200000, ifmr=ifmr.IFMR_Spera15())
+                                       1000000, ifmr=ifmr.IFMR_Spera15())
     assert np.abs(clus_1['systemMass'].sum() - clus_2['systemMass'].sum()) == approx(0, rel=10**-2)
     # Now trying this with the Kennicutt 1983 from Figure 3 of my lunch talk.
     Kennicutt_IMF = imf.imf.Kennicutt1983(multiplicity = imf.multiplicity.
                                           MultiplicityResolvedDK())
     clus_1 = synthetic.Cluster_w_Binaries(iso1, Kennicutt_IMF,
-                                      200000, ifmr=ifmr.IFMR_Spera15())
+                                      1000000, ifmr=ifmr.IFMR_Spera15())
     clus_2 = synthetic.ResolvedCluster(iso2, custom_IMF,
-                                       200000, ifmr=ifmr.IFMR_Spera15())
+                                       1000000, ifmr=ifmr.IFMR_Spera15())
     assert np.abs(clus_1['systemMass'].sum() - clus_2['systemMass'].sum()) == approx(0, rel=10**-2)
     
+
+def test_third_figure_revised():
+    """ In this test I will try to make sure that
+    BPASS clusters' KLF's do not deviate too widely
+    from that of the Observed KLF from the Young Galactic Center Stars
+    from Lu et al. 2013. (around 6 million years of age, solar metallicity,
+    8000 parsecs from the Earth, and 2.7 AKs extinction).
+    For our purposes, we want to make sure that the Observed
+    and BPASS counterpart's KLF's are no farther apart than 0.25.
+    We may also inspect the KLF of the BPASS and MIST v.1. cluster
+    and also the overall initial mass of both BPASS and MIST v1 counterparts
+    to the MIST cluster.
+    Big Change Made: Use of MultiplicityUnresolved (Lu et. al 2013)
+    Use of Merged Models
+    IFMR = None is used
+    """
+
+    result = load_klf_by_radius(mask_for_log=True)
+    magBin = result.Kp[1] - result.Kp[0]
+    idx = np.where(result.Kp < 16)[0]
+    klf_mag_bins = np.arange(9.0, 17, 1.0)
+    binsKp = klf_mag_bins
+    binEdges = binsKp[0:-1] + (binsKp[1:] - binsKp[0:-1]) / 2.0
+
+
+    BPASS_iso = synthetic.Isochrone_Binary(6.78, 2.7,
+                                           8000, 0.0,
+                                           filters=['nirc2,Kp'])
+   # If we want to use control group, let's use multiplicity = None.
+    custom_IMF = imf.imf.IMF_broken_powerlaw(np.array([1, 150]),
+                                             np.array([-1.7]),
+                                             multiplicity=
+                                             (imf.multiplicity.
+                                              MultiplicityUnresolved()))
+    BPASS_Cluster = synthetic.Cluster_w_Binaries(BPASS_iso, custom_IMF,
+                                                 1000000,
+                                                 ifmr=None)
     
+    area = 150.  # arcsec^2
+    clus_mass = 1000000
+    # Think of how many stars per area would there be in a cluster
+    # similar to BPASS_Cluster
+    # but with lower mass. We use scaling.
+    scaleFactorBPS = ((17000 /
+                       clus_mass) /
+                      area)
+    Merged_iso = synthetic.IsochronePhot(6.78, 2.7, 8000, 0.0,
+                                       evo_model=evolution.MergedBaraffePisaEkstromParsec(),
+                                       recomp=False,
+                                       filters=['nirc2,Kp'])
+    Merged_iso = synthetic.IsochronePhot(6.78, 2.7, 8000, 0.0,
+                                         recomp=False,
+                                         filters=['nirc2,Kp'])
+    Mist_Cluster = synthetic.ResolvedCluster(MIST_iso, custom_IMF,
+                                             1000000,
+                                             ifmr=None)
+    Merged_Cluster = synthetic.ResolvedCluster(Merged_iso, custom_IMF,
+                                             1000000,
+                                             ifmr=None)
+    totl_mist =(MIST_Cluster.star_systems['m_nirc2_Kp']
+            [np.where((MIST_Cluster.star_systems['isWR'] == 0))[0]])
+    totl_merged =(Merged_Cluster.star_systems['m_nirc2_Kp']
+                  [np.where((Merged_Cluster.star_systems['isWR'] == 0))[0]])
+    totl_BPASS = (BPASS_Cluster.star_systems['m_nirc2_Kp']
+               [np.where((~BPASS_Cluster.star_systems['isWR']))[0]])
+    mist_scale = scaleFactorBPS
+    merged_scale = mist_scale
+    weightsMST = np.array([1.0 for x in totl_mist])
+    weightsMST *= mist_scale
+    weightsBPS = np.array([1.0 for x in totl_BPASS])
+    weightsBPS *= scaleFactorBPS
+    weightsMerged = np.array([1.0 for x in totl_merged])
+    weightsMerged *= merged_scale
+    # Binning the K' magnitudes into their respective bins
+    # And plotting KLF's of BPASS_Cluster, MIST clusters, and
+    # Observed KLF
+    # n, bins, patches are exactly the same objects that are
+    # described as outputs of matplotlib.hist
+    # Right below: BPASS cluster KLF histogram's bin values, bin-edges, and patches
+    n, bins, patches = py.hist(totl_mist, bins=binEdges, histtype='step',
+                               weights=weightsMST, color='green', label='Merged Model',
+                               align='mid', linewidth=1.5)
+    # Right below: BPASS cluster KLF histogram's bin values, bin-edges, and patches
+    n2, bins2, patches2 = py.hist(totl_BPASS, bins=binEdges, histtype='step',
+                                  weights=weightsBPS, color='blue',
+                                  label='(BPASS Model KLF)',
+                                  align='mid', linewidth=1.5)
+    n3, bins3, patches3 = py.hist(totl_merged, bins=binEdges, histtype='step',
+                                  weights=weightsMerged, color='blue',
+                                  label='(BPASS Model KLF)',
+                                  align='mid', linewidth=1.5)
+    py.errorbar(result.Kp[idx], result.KLF_ext_cmp_sp_im_noWR[idx],
+                fmt='ro-', xerr=magBin/2.0, capsize=0, linewidth=2)
+    py.errorbar(result.Kp[idx] ,result.KLF_ext_cmp_sp_im_noWR[idx],
+                fmt='ro-', yerr=result.eKLF_ext_cmp_sp_im_noWR[idx],
+                linewidth=2,
+                label='Observed')
+    py.xlim(8.5, 15.5)
+    py.xlabel('Kp magnitude')
+    py.ylabel("stars / (arcsecond^2 mag)")
+    py.title("KLF's at Age = %d Myr" % (10**(6.78 - 6)), fontsize=14)
+    py.legend(loc='upper left', numpoints=1)
+    py.savefig('Comparisons_w_Real_Data.png')
+    # Now compare differences in the heights of BPASS cluster KLF and given KLF
+    for x in range(len(binEdges)):
+        assert np.abs(result.KLF_ext_cmp_sp_im_noWR[idx][x] - n[x]) == approx(0, abs=0.25)
+    print("BPASS IMF does not seem too far off when compared to the BPASS IMF")
+    # Now compare differences in the BPASS and MIST KLF's (or values
+    # of bins in histograms for both KLF's)
+    for x in range(len(binEdges)):
+        assert np.abs(n2[x] - n[x]) == approx(0, abs=0.125)
+        assert np.abs(n2[x] - n3[x]) == approx(0, abs=0.125)
+    # Now we try to generate a figure closer to the Figure 1 of Lu et al 2013
+    custom_IMF2 = 
+    BPASS_Cluster = synthetic.Cluster_w_Binaries(BPASS_iso, custom_IMF,
+                                                 1000000,
+                                                 ifmr=None)
     
+def test_third_figure_w_Tab2Sol1():
+    """ In this test I will try to make sure that
+    BPASS clusters' KLF's do not deviate too widely
+    from that of the Observed KLF from the Young Galactic Center Stars
+    from Lu et al. 2013. (around 6 million years of age, solar metallicity,
+    8000 parsecs from the Earth, and 2.7 AKs extinction).
+    For our purposes, we want to make sure that the Observed
+    and BPASS counterpart's KLF's are no farther apart than 0.25.
+    We may also inspect the KLF of the BPASS and MIST v.1. cluster
+    and also the overall initial mass of both BPASS and MIST v1 counterparts
+    to the MIST cluster.
+    Big Change Made: Use of MultiplicityUnresolved (Lu et. al 2013)
+    Use of Merged Models
+    IFMR = None is used
+    Using Table 2 Solution 1 from Lu et. al 2013
+    """
+
+    result = load_klf_by_radius(mask_for_log=True)
+    magBin = result.Kp[1] - result.Kp[0]
+    idx = np.where(result.Kp < 16)[0]
+    klf_mag_bins = np.arange(9.0, 17, 1.0)
+    binsKp = klf_mag_bins
+    binEdges = binsKp[0:-1] + (binsKp[1:] - binsKp[0:-1]) / 2.0
+
+
+    BPASS_iso = synthetic.Isochrone_Binary(6.78, 2.7,
+                                           8000, 0.0,
+                                           filters=['nirc2,Kp'])
+   # If we want to use control group, let's use multiplicity = None.
+    custom_IMF = imf.imf.IMF_broken_powerlaw(np.array([1, 150]),
+                                             np.array([-1.7]),
+                                             multiplicity=
+                                             (imf.multiplicity.
+                                              MultiplicityUnresolved()))
+    BPASS_Cluster = synthetic.Cluster_w_Binaries(BPASS_iso, custom_IMF,
+                                                 1000000,
+                                                 ifmr=None)
     
-    
-    
+    area = 150.  # arcsec^2
+    clus_mass = 1000000
+    # Think of how many stars per area would there be in a cluster
+    # similar to BPASS_Cluster
+    # but with lower mass. We use scaling.
+    scaleFactorBPS = ((17000 /
+                       clus_mass) /
+                      area)
+    Merged_iso = synthetic.IsochronePhot(6.62, 2.7, 8000, 0.0,
+                                       evo_model=evolution.MergedBaraffePisaEkstromParsec(),
+                                       recomp=False,
+                                       filters=['nirc2,Kp'])
+    Merged_iso = synthetic.IsochronePhot(6.62, 2.7, 8000, 0.0,
+                                         recomp=False,
+                                         filters=['nirc2,Kp'])
+    Mist_Cluster = synthetic.ResolvedCluster(MIST_iso, custom_IMF,
+                                             1000000,
+                                             ifmr=None)
+    Merged_Cluster = synthetic.ResolvedCluster(Merged_iso, custom_IMF,
+                                             1000000,
+                                             ifmr=None)
+    totl_mist =(MIST_Cluster.star_systems['m_nirc2_Kp']
+            [np.where((MIST_Cluster.star_systems['isWR'] == 0))[0]])
+    totl_merged =(Merged_Cluster.star_systems['m_nirc2_Kp']
+                  [np.where((Merged_Cluster.star_systems['isWR'] == 0))[0]])
+    totl_BPASS = (BPASS_Cluster.star_systems['m_nirc2_Kp']
+               [np.where((~BPASS_Cluster.star_systems['isWR']))[0]])
+    mist_scale = scaleFactorBPS
+    merged_scale = mist_scale
+    weightsMST = np.array([1.0 for x in totl_mist])
+    weightsMST *= mist_scale
+    weightsBPS = np.array([1.0 for x in totl_BPASS])
+    weightsBPS *= scaleFactorBPS
+    weightsMerged = np.array([1.0 for x in totl_merged])
+    weightsMerged *= merged_scale
+    # Binning the K' magnitudes into their respective bins
+    # And plotting KLF's of BPASS_Cluster, MIST clusters, and
+    # Observed KLF
+    # n, bins, patches are exactly the same objects that are
+    # described as outputs of matplotlib.hist
+    # Right below: BPASS cluster KLF histogram's bin values, bin-edges, and patches
+    n, bins, patches = py.hist(totl_mist, bins=binEdges, histtype='step',
+                               weights=weightsMST, color='green', label='Merged Model',
+                               align='mid', linewidth=1.5)
+    # Right below: BPASS cluster KLF histogram's bin values, bin-edges, and patches
+    n2, bins2, patches2 = py.hist(totl_BPASS, bins=binEdges, histtype='step',
+                                  weights=weightsBPS, color='blue',
+                                  label='(BPASS Model KLF)',
+                                  align='mid', linewidth=1.5)
+    n3, bins3, patches3 = py.hist(totl_merged, bins=binEdges, histtype='step',
+                                  weights=weightsMerged, color='blue',
+                                  label='(BPASS Model KLF)',
+                                  align='mid', linewidth=1.5)
+    py.errorbar(result.Kp[idx], result.KLF_ext_cmp_sp_im_noWR[idx],
+                fmt='ro-', xerr=magBin/2.0, capsize=0, linewidth=2)
+    py.errorbar(result.Kp[idx] ,result.KLF_ext_cmp_sp_im_noWR[idx],
+                fmt='ro-', yerr=result.eKLF_ext_cmp_sp_im_noWR[idx],
+                linewidth=2,
+                label='Observed')
+    py.xlim(8.5, 15.5)
+    py.xlabel('Kp magnitude')
+    py.ylabel("stars / (arcsecond^2 mag)")
+    py.title("KLF's at Age = %d Myr" % (10**(6.62 - 6)), fontsize=14)
+    py.legend(loc='upper left', numpoints=1)
+    py.savefig('Comparisons_w_Real_Data.png')
+    # Now compare differences in the heights of BPASS cluster KLF and given KLF
+    for x in range(len(binEdges)):
+        assert np.abs(result.KLF_ext_cmp_sp_im_noWR[idx][x] - n[x]) == approx(0, abs=0.25)
+    print("BPASS IMF does not seem too far off when compared to the BPASS IMF")
+    # Now compare differences in the BPASS and MIST KLF's (or values
+    # of bins in histograms for both KLF's)
+    for x in range(len(binEdges)):
+        assert np.abs(n2[x] - n[x]) == approx(0, abs=0.1)
+        assert np.abs(n2[x] - n3[x]) == approx(0, abs=0.1)
     
