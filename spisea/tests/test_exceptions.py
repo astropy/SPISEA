6ce905963093d8d13b7c1e9e35466f8919915c16
Matt Hosek <mwhosek@ifa.hawaii.edu>
2022-05-23 13:37:53 -0700

    Adding tests for ModelMismatch exception

---
+++ spisea/tests/test_exceptions.py
@@ -0,0 +1,43 @@
+from spisea import evolution, exceptions
+import copy
+import os
+import pdb
+
+
+def test_grid_number_exception():
+    """
+    Make sure check_evo_grid_number works as expected
+    """
+    #----Case 1: installed model grid is too low (raise error)----#
+    # Get installed grid num
+    models_dir = '{0}/evolution/'.format(os.environ['SPISEA_MODELS'])
+    installed_grid = evolution.get_installed_grid_num(models_dir)
+
+    # Make sure required grid is too large
+    required_grid = installed_grid + 1
+
+    # Make sure correct error is raised, otherwise fail
+    try:
+        evolution.check_evo_grid_number(required_grid, models_dir)
+    except exceptions.ModelMismatch:
+        pass
+    else:
+        raise Exception('check_evo_grid_number failed')
+
+    # Case 2: installed model grid is same (no error)
+    required_grid = copy.deepcopy(installed_grid)
+
+    evolution.check_evo_grid_number(required_grid, models_dir)
+
+    # Case 3: installed model grid is higher than required grid (no error)
+    required_grid = installed_grid - 1.0
+
+    evolution.check_evo_grid_number(required_grid, models_dir)    
+
+    return
+
+
+
+        
+
+    
