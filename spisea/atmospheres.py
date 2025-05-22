b061039014e1d589da732066f71f8824af45ab80
Macy Huston <mhuston@berkeley.edu>
2025-05-22 14:10:06 -0700

    more info in print statement

--- spisea/atmospheres.py
+++ spisea/atmospheres.py
@@ -80,11 +80,11 @@ def get_atmosphere_bounds(model_dir, metallicity=0, temperature=20000, gravity=4
     
     # Print out changes, if any
     if temperature_new != temperature:
-        teff_msg = 'Changing to T={0:6.0f} for T={1:6.0f} logg={2:4.2f}'
+        teff_msg = 'Changing to T={0:6.0f} for met={1:4.2f} T={1:6.0f} logg={2:4.2f}'
         print( teff_msg.format(temperature_new, temperature, gravity))
     
     if gravity_new != gravity:
-        logg_msg = 'Changing to logg={0:4.2f} for T={1:6.0f} logg={2:4.2f}'
+        logg_msg = 'Changing to logg={0:4.2f} for met={1:4.2f} T={1:6.0f} logg={2:4.2f}'
         print( logg_msg.format(gravity_new, temperature, gravity))
 
     if metallicity_new != metallicity:
