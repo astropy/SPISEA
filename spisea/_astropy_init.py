c9f4f6f895af9abfa854d0f960a7af920b8dee3c
Matt Hosek <mwhosek@nott.astro.ucla.edu>
2025-10-23 10:47:06 -0700

    figuring out tests

--- spisea/_astropy_init.py
+++
@@ -1,8 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-__all__ = ['__version__']
-
-try:
-    from .version import version as __version__
-except ImportError:
-    __version__ = ''
